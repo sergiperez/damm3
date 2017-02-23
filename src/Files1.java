4d9be3f833b54b8cdb18445ce3a497555a593a4f
sergi.perez <sergi.perez@DESKTOP-RJ55473>
2017-02-23 17:31:06 +0100

    Solució arxius activitat 1

---
+++ src/Files1.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Files1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File f = new File("C:\\Users\\sergi.perez\\Desktop\\Reals.txt");
+		try{
+			Scanner lector = new Scanner(f);
+			//Llegir a línia a línia l'arxiu
+			double max=0;
+			while(lector.hasNextDouble()){
+				//double n = lector.nextDouble();
+				if (max < lector.nextDouble()){
+					max=lector.nextDouble();
+				}
+			}
+			System.out.println("El màxim és "+max);
+			lector.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
